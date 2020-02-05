367e36fd7d5f39f22a349468d3a50dd8a002d6e8
Konstantin Budunov <kobudunov@neoflex.local>
2020-02-05 18:16:13 +0400

    kafka-producer example was added

---
+++ kafka-producer/build.sbt
@@ -0,0 +1,9 @@
+name := "KafkaProducer"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+
+libraryDependencies += "com.typesafe.akka" %% "akka-stream-kafka" % "1.0-RC1"
+libraryDependencies += "com.github.seratch" %% "scalikejdbc" % "1.2.2"
---
+++ kafka-producer/src/main/scala/KafkaProducer.scala
@@ -0,0 +1,26 @@
+import akka.Done
+import akka.actor.ActorSystem
+import akka.kafka.ProducerSettings
+import akka.kafka.scaladsl.Producer
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+import com.typesafe.config.ConfigFactory
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
+
+import scala.concurrent.Future
+
+object KafkaProducer extends App {
+  implicit val system = ActorSystem("QuickStart")
+  implicit val materializer = ActorMaterializer()
+  val config = ConfigFactory.load.getConfig("akka.kafka.producer")
+  val producerSettings =
+    ProducerSettings(config, new StringSerializer, new StringSerializer)
+      .withBootstrapServers("http://localhost:9092")
+  val producerSink: Future[Done] =
+    Source(1 to 10)
+      .map(_.toString)
+      .map(value => new ProducerRecord[String, String]("test2", "Record:: " + value))
+      .runWith(Producer.plainSink(producerSettings))
+  println("************ Message produced ************")
+}
